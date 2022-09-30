2f0fe7687c3137c9bed2e169519c7037d791eea8
anton.peraviortau <anton.peraviortau@jobilla.com>
2022-09-30 13:50:42 +0200

    first commit

---
+++ udemy_swiftuiApp.swift
@@ -0,0 +1,18 @@
+//
+//  udemy_swiftuiApp.swift
+//  udemy_swiftui
+//
+//  Created by Anton Peraviortau on 03/09/2022.
+//
+//
+
+import SwiftUI
+
+@main
+struct udemy_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
